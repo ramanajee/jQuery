ca5d0392bacb865dc07f84603091cc61515c1211
Ramanajee Kandregula <ramanajee516@live.in>
2015-09-30 09:58:10 +0530

    jQueryLoad

---
+++ jQueryDemos/jQueryDemos/jQueryScripts/master.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="C:\Users\RAMANAJEE\Source\Repos\jQuery\jQueryDemos\jQueryDemos\Scripts/jquery-2.1.4.js" />
+
+
+$(document).ready(function() {
+    alert('ready method executed')
+})
+
+$(window).load(function() {
+    alert('windows.load method executed');
+});
+
+$('#iframe').load(function () {
+    alert('iframe.load metghod executed')
+});
