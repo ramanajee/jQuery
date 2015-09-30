48e83491f01cc1d7b20be73922efb5730fe5f82f
Ramanajee Kandregula <ramanajee516@live.in>
2015-10-01 00:18:43 +0530

    javascript Listner

---
+++ jQueryDemos/jQueryDemos/App/master.js
@@ -0,0 +1,21 @@
+﻿var Test = Test || {};
+(function (Test) {
+    document.getElementById('increase').addEventListener('click', function () {
+        var element = document.getElementById('txtValue');
+        element.value = parseInt(element.value) + 1;
+    });
+    document.getElementById('decrease').addEventListener('click', function () {
+        var element = document.getElementById('txtValue');
+        element.value = parseInt(element.value) - 1;
+    });
+
+    document.getElementById('colorBox').addEventListener('click', function () {
+        // 16777215 == ffffff
+        this.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        this.style.borderColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    })
+
+    setInterval(function () {
+        document.getElementById('colorBox').click();
+    }, 1000);
+})();
