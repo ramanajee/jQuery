94e6197bc7513bb277e289c774d76b6547c55bb7
Ramanajee Kandregula <ramanajee516@live.in>
2015-10-06 07:23:34 +0530

    Month added

--- jQueryDemos/jQueryDemos/App/clock.js
+++ jQueryDemos/jQueryDemos/App/clock.js
@@ -3,9 +3,8 @@
     var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
     var now = new Date();
-    now.getDate();
     var t = d.toLocaleTimeString()
-    document.getElementById("clock-large").innerHTML = t + "<br>" + days[now.getDay()];
+    document.getElementById("clock-large").innerHTML = t + "<br>" + days[now.getDay()] + "&nbsp;&nbsp;" + months[now.getMonth()];
 
 }
 setInterval(function () { digitalClock() }, 1000)
