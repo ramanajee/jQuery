83d6a28593775e4c585f4d3663e3d5e47e28dfdc
Ramanajee Kandregula <ramanajee516@live.in>
2015-10-06 06:52:28 +0530

    Digital Clock

---
+++ jQueryDemos/jQueryDemos/App/clock.js
@@ -0,0 +1,11 @@
+﻿function digitalClock() {
+    var d = new Date()
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+    var now = new Date();
+    now.getDate();
+    var t = d.toLocaleTimeString()
+    document.getElementById("clock-large").innerHTML = t + "<br>" + days[now.getDay()];
+
+}
+setInterval(function () { digitalClock() }, 1000)
